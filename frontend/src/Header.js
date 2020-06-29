570d14d18e27563b0c4267fd9ab8a1ecad5ec424
Tomas Gomes <tloureirog@gmail.com>
2020-06-29 00:07:26 -0300

    Inicio do frontend

---
+++ frontend/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header({ children }){
+  return (
+    <header>
+      {/* <h1>{props.title}</h1> */}
+      <h1>{children}</h1>
+    </header>
+  );
+}
+
